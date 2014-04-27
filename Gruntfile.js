b53092c6b3a30bad2dee2f1b1dbcb87c71c00155
Leonardo <leotasco@gmail.com>
2014-04-27 19:35:03 +0000

    Add grunt-hub and update travis file

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+/**
+ * Example Grunt Hub
+ *
+ * Edit the hub.all.src to point to your Gruntfile locations.
+ * Then run `grunt`.
+ */
+module.exports = function(grunt) {
+  'use strict';
+
+  grunt.initConfig({
+    hub: {
+      all: {
+        src: ['./api/Gruntfile.js'],
+        tasks: ['default'],
+      },
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-hub');
+
+  grunt.registerTask('default', ['hub']);
+};
