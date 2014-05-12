8207e042b469abe93c4024c72eacc53760f0bd44
Leonardo <leotasco@gmail.com>
2014-05-12 12:37:21 +0000

    delete misc folder

--- misc/api_20140505/app/controllers/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  athletes : require('./athletes_controller')
-};
--- misc/api_20140505/test/support/_instrumentForCoverage.js
+++
@@ -1,6 +0,0 @@
-var path = require('path');
-var appDir = path.join(__dirname, '..', '..', 'app');
-
-require('blanket')({
-  pattern: appDir
-});
